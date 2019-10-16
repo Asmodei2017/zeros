e151d80e768e4770d25e524024140c21769b53c3
Asmodei2017 <p.zabavskiy@tut.by>
2019-10-16 19:02:48 +0200

    fix: add new version task zeros

--- src/index.js
+++ src/index.js
@@ -1,40 +1,21 @@
-module.exports = function zeros(expression) {
-				
-  function factorial(n) {
-      return (n != 1) ? n * factorial(n - 1) : 1;
-  }
-    
-  function factorial2(n) {
-    return (n != 1) ? n * factorial(n - 2) : 1;
-  }   
-    
-  let arr = expression.split('*');
-  let result = 1;
-  let num = [];
-  let strNum;   
-    
-  for(let i = 0; i < arr.length; i++){
-    strNum = arr[i].split('');
-    if(strNum[strNum.length - 1] === '!' && strNum[strNum.length - 2] === '!'){
-        
-      for(let x = 0; x < strNum.length - 2; x++){
-        num.push(strNum[x]);
-      }
-      result *= factorial2(Number(num.join('')));	
-      num = [];        
-    }
-    for(let y = 0; y < strNum.length - 1; y++){
-      num.push(strNum[y]);
-    }
-    result *= factorial(Number(num.join('')));
-    num = [];
-  }
-  let count;
-  for (let q = 0; q < result.length; q++){
-    if(result[q] === 0){
-      count++;
-    }
-  }
-    
-  return count;
+module.exports = function zeros(expression){
+	let dividers = {'5': 0, '2': 0};
+	expression.split('*').forEach(factorial => {
+		let isParity = (factorial.includes('!!')) ? 2 : 1;
+		Object.keys(dividers).forEach(divider => {
+			for(let num = parseInt(factorial); num >= divider; num -= isParity){
+				if(!(num % divider)) {
+					let count = num;
+					while(!(count % divider)) {
+						count = count / divider;
+						divider === '5' ? dividers['5'] += 1 : dividers['2'] += 1;
+					}
+				}
+			}
+		});
+		
+	});
+	return Math.min(dividers['5'], dividers['2']);
 }
+    
+
